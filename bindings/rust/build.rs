a5294d4c1e8973a4bc4a18ab1be2b6a1cd418d3f
dengqi <dengqi935@outlook.com>
2025-03-27 17:20:47 +0800

    fix: udpate build.rs

--- bindings/rust/build.rs
+++ bindings/rust/build.rs
@@ -6,11 +6,13 @@ fn main() {
         .flag("-std=c99")
         .flag("-Wall")
         .flag("-Wextra")
-        .compile("libjetpwmon");
-
-    println!("cargo:rustc-link-lib=dylib=jetpwmon");
+        .static_flag(true)
+        .compile("jetpwmon");
 
     println!("cargo:rustc-link-search=native={}", std::env::var("OUT_DIR").unwrap());
+    println!("cargo:rustc-link-lib=static=jetpwmon");
+    println!("cargo:rustc-link-lib=pthread");
+
 
     println!("cargo:rerun-if-changed=src/lib.rs");
     println!("cargo:rerun-if-changed=vendor/src/jetpwmon.c");
